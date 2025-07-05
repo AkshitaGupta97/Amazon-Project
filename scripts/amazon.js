2bf7eb14acbcc462e2bce031f88ef33886727eb2
Akshita Gupta 97 <gungunakshita37gupta973@gmail.com>
2025-07-05 15:53:21 +0530

    products.js data

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,45 +1,14 @@
 
-const products = [
-    {
-        imgage: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-        name: " Plain Cotton T-Shirt",
-        rating: {
-            stars: 4.5,
-            count: 120
-        },
-        priceCents: 1090,
-    },
 
-    {
-        imgage: "images/products/intermediate-composite-basketball.jpg",
-        name: " Basketball",
-        rating: {
-            stars: 3.5,
-            count: 150
-        },
-        priceCents: 2095,
-    },
-
-    {
-        imgage: "images/products/athletic-cotton-socks-6-pairs.jpg",
-        name: " Cotton Socks - 6 Pairs",
-        rating: {
-            stars: 5.0,
-            count: 200
-        },
-        priceCents: 799,
-    }
-
-];
 // This code will generate the HTML for each product and append it to the container
 let productsHTML = '';
 
 products.forEach((product) => {
-    productsHTML +=  `
+    productsHTML +=  ` 
     
         <div class="product-container">
           <div class="product-image-container">
-            <img class="product-image" src="${product.imgage}" alt="${product.name}">
+            <img class="product-image" src="${product.image}" alt="${product.name}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
@@ -54,7 +23,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            ${(product.priceCents / 100).toFixed(2)}  // to convert into decimal
+            ${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
