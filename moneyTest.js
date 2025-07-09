4c70dd752ca30ecf5fb7daafbe65e1f87670eab5
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-07-09 06:09:25 +0530

    jasmine testcase

---
+++ moneyTest.js
@@ -0,0 +1,12 @@
+
+import {formatCurrency} from '../scripts/utility_code/formatMoney.js'
+
+// to create a test suite - > in jasmine use describe()
+// it() -> is used to describe a function
+// expect -> is used to compare other function with a value, it gives us objexct to compare the value
+
+describe('Test Suit: formatCurrency', () => {
+    it('converts cents into dollars', () => {
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+})
--- scripts/utility_code/formatMoney.js
+++ scripts/utility_code/formatMoney.js
@@ -1,6 +1,6 @@
 
 export function formatCurrency(priceCents) {
-  return (Math.round(priceCents) /100 ).toFixed(2);
+  return (d(priceCents) /100 ).toFixed(2);
 }   
 
 export default formatCurrency; // Exporting the function as default so it can be imported without curly braces
