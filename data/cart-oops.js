b352ed7159476951ca76e1919c9b347824d12fb6
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-07-09 21:14:04 +0530

    oop multiple cart

--- data/cart-oops.js
+++ data/cart-oops.js
@@ -1,162 +1,90 @@
 
 import { deliveryOptions } from './deliveryOption.js';
 
-// this is oops concept
+function Cart(localStorageKey){
+    const cart = {
+        cartItems: undefined,
 
-const cart = {
-    cartItems: undefined,
+        loadFromStorage: function(){
+            // here this represent cart
+            this.cartItems = JSON.parse(localStorage.getItem(localStorageKey)) || [];
 
-    loadFromStorage: function(){
-        // here this represent cart
-        this.cartItems = JSON.parse(localStorage.getItem('cart-oop')) || [];
+            if(!Array.isArray(this.cartItems)) {
+                this.cartItems = [
+                {
+                    productId: '1',
+                    deliveryOptionId: '1',
+                    quantity: 2
+                },
+                {
+                    productId: '2',
+                    deliveryOptionId: '2',
+                    quantity: 1
+                },
+                ]
+            }
+        },
+        saveToStorage() {
+            localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems));
+        },
+        addToCart(productId){
+            let matchingItem;
+            this.cartItems.forEach((cartItem) => {
+                if(productId === cartItem.productId){
+                matchingItem = cartItem;
+                }
+            });
 
-        if(!Array.isArray(this.cartItems)) {
-            this.cartItems = [
-            {
-                productId: '1',
-                deliveryOptionId: '1',
-                quantity: 2
-            },
-            {
-                productId: '2',
-                deliveryOptionId: '2',
+            if(matchingItem){
+                matchingItem.quantity += 1;
+            }
+            else {
+                this.cartItems.push({
+                productId: productId,
+                deliveryOptionId: '1', // Default delivery option
                 quantity: 1
-            },
-            ]
-        }
-    },
-    saveToStorage() {
-        localStorage.setItem('cart-oop', JSON.stringify(this.cartItems));
-    },
-    addToCart(productId){
-        let matchingItem;
-        this.cartItems.forEach((cartItem) => {
-            if(productId === cartItem.productId){
-            matchingItem = cartItem;
+                })
             }
-        });
+            this.saveToStorage();
+        },
+        removeFromCart(productId){
+            const newCart = [];
+            
+            cart.forEach((cartItem) => {
+                if(cartItem.productId !== productId){
+                newCart.push(cartItem);
+                }
+            });
 
-        if(matchingItem){
-            matchingItem.quantity += 1;
+            cart = newCart;
+            this.saveToStorage();
+        },
+        updateDeliveryOption(productId, deliveryOptionId) {
+            let matchingItem;
+
+            cart.forEach((cartItem) => {
+                if(productId === cartItem.productId){
+                matchingItem = cartItem;
+                }
+            });
+
+            matchingItem.deliveryOptionId = deliveryOptionId;
+            this.saveToStorage();
         }
-        else {
-            this.cartItems.push({
-            productId: productId,
-            deliveryOptionId: '1', // Default delivery option
-            quantity: 1
-            })
-        }
-        this.saveToStorage();
-    },
-    removeFromCart(productId){
-        const newCart = [];
-        
-        cart.forEach((cartItem) => {
-            if(cartItem.productId !== productId){
-            newCart.push(cartItem);
-            }
-        });
 
-        cart = newCart;
-        this.saveToStorage();
-    },
-    updateDeliveryOption(productId, deliveryOptionId) {
-        let matchingItem;
-
-        cart.forEach((cartItem) => {
-            if(productId === cartItem.productId){
-            matchingItem = cartItem;
-            }
-        });
-
-        matchingItem.deliveryOptionId = deliveryOptionId;
-        this.saveToStorage();
     }
-
+    return cart;
 }
+
+// this is oops concept
+const cart = Cart('cart-oop');
+
+const businessCart = Cart('cart-business');
 
 cart.loadFromStorage();
+businessCart.loadFromStorage();
+
+console.log(cart);
+console.log(businessCart);
 
 // oops are important as it provide multiple uses of codes, makes easy to create multiple objects
-
-const cartBusiness = {
-    cartItems: undefined,
-
-    loadFromStorage: function(){
-        // here this represent cart
-        this.cartItems = JSON.parse(localStorage.getItem('cart-business')) || [];
-
-        if(!Array.isArray(this.cartItems)) {
-            this.cartItems = [
-            {
-                productId: '1',
-                deliveryOptionId: '1',
-                quantity: 2
-            },
-            {
-                productId: '2',
-                deliveryOptionId: '2',
-                quantity: 1
-            },
-            ]
-        }
-    },
-    saveToStorage() {
-        localStorage.setItem('cart-business', JSON.stringify(this.cartItems));
-    },
-    addToCart(productId){
-        let matchingItem;
-        this.cartItems.forEach((cartItem) => {
-            if(productId === cartItem.productId){
-            matchingItem = cartItem;
-            }
-        });
-
-        if(matchingItem){
-            matchingItem.quantity += 1;
-        }
-        else {
-            this.cartItems.push({
-            productId: productId,
-            deliveryOptionId: '1', // Default delivery option
-            quantity: 1
-            })
-        }
-        this.saveToStorage();
-    },
-    removeFromCart(productId){
-        const newCart = [];
-        
-        cart.forEach((cartItem) => {
-            if(cartItem.productId !== productId){
-            newCart.push(cartItem);
-            }
-        });
-
-        cart = newCart;
-        this.saveToStorage();
-    },
-    updateDeliveryOption(productId, deliveryOptionId) {
-        let matchingItem;
-
-        cart.forEach((cartItem) => {
-            if(productId === cartItem.productId){
-            matchingItem = cartItem;
-            }
-        });
-
-        matchingItem.deliveryOptionId = deliveryOptionId;
-        this.saveToStorage();
-    }
-
-}
-cartBusiness.loadFromStorage();
-
-/*
-console.log("Business Cart = ")
-console.log(cartBusiness);
-console.log("cart display = ")
-console.log(cart);
-*/
-
-
