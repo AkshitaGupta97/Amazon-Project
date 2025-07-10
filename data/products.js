8c241c7d08e104c9738e43fa0b8627ed8674ad45
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-07-10 06:12:45 +0530

    additional product.js changes

--- data/products.js
+++ data/products.js
@@ -37,27 +37,6 @@ class Product {
 
 // $${(this.priceCents / 100).toFixed(2)}
 
-const product1 = new Product(
-  {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: [
-      "socks",
-      "sports",
-      "apparel"
-    ]
-  },
-);
-console.log(product1);
-
-
-
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -721,4 +700,3 @@ export const products = [
   return new Product(productDetails)
 });
 
-console.log(products);
